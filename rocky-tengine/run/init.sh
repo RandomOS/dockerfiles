93289bd6618ebce1ff1d940256555b0c021bc66a
RandomK <gyqmstc@sina.com>
2023-02-17 00:41:31 +0800

    update rocky-tengine

---
+++ rocky-tengine/run/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $(arch) == "aarch64" ]]; then
+    arch="arm64"
+elif [[ $(arch) == "armv7l" ]]; then
+    arch="arm7"
+else
+    arch="amd64"
+fi
+
+wget -q -O /usr/sbin/nginx.gz http://rocky.evai.pl/ftp/bin/alpine/${arch}/tengine.gz \
+    && gzip -d /usr/sbin/nginx.gz \
+    && chmod +x /usr/sbin/nginx \
+    && mkdir -p /var/log/nginx \
+    && mkdir -p /var/lib/nginx/body \
+    && mkdir -p /var/lib/nginx/fastcgi \
+    && mkdir -p /var/lib/nginx/proxy \
+    && mkdir -p /var/lib/nginx/scgi \
+    && mkdir -p /var/lib/nginx/uwsgi \
+    && mkdir -p /usr/lib/nginx/modules
