e059d272d580a2d9ea213760cd91768dcdecf04d
RandomK <gyqmstc@sina.com>
2024-07-12 23:24:41 +0800

    update qemu

--- qemu/run/init.sh
+++ qemu/run/init.sh
@@ -4,7 +4,7 @@ arch=$(uname -m)
 
 apk update
 apk add --no-cache bash bash-completion curl wget vim tzdata tini iproute2
-apk add --no-cache ovmf qemu-img qemu-system-${arch} \
+apk add --no-cache qemu-img qemu-system-${arch} \
     qemu-hw-display-virtio-vga \
     qemu-hw-display-virtio-vga-gl \
     qemu-hw-display-virtio-gpu \
@@ -14,9 +14,13 @@ apk add --no-cache ovmf qemu-img qemu-system-${arch} \
     qemu-chardev-spice \
     mesa-dri-gallium
 
-if [[ ${arch} == "x86_64" ]]; then
+if [[ ${arch} == "aarch64" ]]; then
+    apk add --no-cache aavmf
+elif [[ ${arch} == "x86_64" ]]; then
+    apk add --no-cache ovmf
     curl -4skL -o /usr/local/bin/qmp-shell https://github.com/0xef53/qmp-shell/releases/download/v2.0.1/qmp-shell \
         && chmod +x /usr/local/bin/qmp-shell
 fi
 
+apk cache clean
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.huaweicloud.com/g' /etc/apk/repositories
