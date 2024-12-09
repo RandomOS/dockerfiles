538ee95ae1790423a46a6c7111236b2cfa9ba9a2
RandomK <gyqmstc@sina.com>
2024-12-09 10:28:50 +0800

    update alpine version to 3.21

--- alpine-lab/build.sh
+++ alpine-lab/build.sh
@@ -5,4 +5,4 @@ CWD=$(dirname ${SCRIPT})
 
 cd ${CWD}
 
-docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64,linux/arm/v7 -t randomos/alpine-lab:3.20 -t randomos/alpine-lab:latest --push .
+docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64,linux/arm/v7 -t randomos/alpine-lab:3.21 -t randomos/alpine-lab:latest --push .
