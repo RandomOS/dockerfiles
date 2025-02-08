47a0670cf3cd11da172c2c8560f14394124a0374
RandomK <gyqmstc@sina.com>
2025-02-08 11:23:51 +0800

    update openresty

--- openresty/run/init.sh
+++ openresty/run/init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y bash bash-completion curl wget vim locales tzdata iproute2 less procps netcat-openbsd \
+    && apt-get clean \
+    && locale-gen en_US.UTF-8 \
+    && echo $TZ > /etc/timezone \
+    && ln -sf /usr/share/zoneinfo/$TZ /etc/localtime \
+    && dpkg-reconfigure -f noninteractive tzdata
+
 if [[ ! -f /etc/apt/sources.list.orig ]]; then
     cp /etc/apt/sources.list /etc/apt/sources.list.orig
 fi
@@ -9,11 +17,3 @@ sed -i \
     -e '/snapshot.debian.org/d' /etc/apt/sources.list \
     -e 's/deb.debian.org/mirrors.huaweicloud.com/g' /etc/apt/sources.list \
     -e 's/security.debian.org/mirrors.huaweicloud.com/g' /etc/apt/sources.list
-
-apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get install -y bash bash-completion curl wget vim locales tzdata iproute2 less procps netcat-openbsd \
-    && apt-get clean \
-    && locale-gen en_US.UTF-8 \
-    && echo $TZ > /etc/timezone \
-    && ln -sf /usr/share/zoneinfo/$TZ /etc/localtime \
-    && dpkg-reconfigure -f noninteractive tzdata
