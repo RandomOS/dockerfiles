874563431762a2664fb1eb637fd462132ddd1306
RandomK <gyqmstc@sina.com>
2025-04-18 18:55:15 +0800

    update rocky-nginx

--- rocky-tengine/build.sh
+++ rocky-tengine/build.sh
@@ -5,4 +5,4 @@ CWD=$(dirname ${SCRIPT})
 
 cd ${CWD}
 
-docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64 -t randomos/rocky-tengine:latest --push .
+docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64 -t randomos/rocky-tengine:3.1.0 -t randomos/rocky-tengine:latest --push .
