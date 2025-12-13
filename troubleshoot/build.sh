51a4a9923ec9289a5d4eed1ef693ce103d8fd843
RandomK <gyqmstc@sina.com>
2025-12-13 22:49:51 +0800

    update troubleshoot version to 3.23

--- troubleshoot/build.sh
+++ troubleshoot/build.sh
@@ -5,4 +5,4 @@ CWD=$(dirname ${SCRIPT})
 
 cd ${CWD}
 
-docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64,linux/arm/v7 -t randomos/troubleshoot:latest --push .
+docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64,linux/arm/v7 -t randomos/troubleshoot:3.23 -t randomos/troubleshoot:latest --push .
