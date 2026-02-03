bbad98144b2dc0824e24cca5e4bac509e0e27e0d
RandomK <gyqmstc@sina.com>
2026-02-03 23:24:16 +0800

    shfmt scripts

--- openresty/run/init.sh
+++ openresty/run/init.sh
@@ -4,7 +4,7 @@ apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -y bash bash-completion curl wget vim locales tzdata iproute2 less procps netcat-openbsd \
     && apt-get clean \
     && locale-gen en_US.UTF-8 \
-    && echo $TZ > /etc/timezone \
+    && echo $TZ >/etc/timezone \
     && ln -sf /usr/share/zoneinfo/$TZ /etc/localtime \
     && dpkg-reconfigure -f noninteractive tzdata
 
@@ -18,7 +18,7 @@ sed -i \
 
 sed -i 's/^deb/#deb/' /etc/apt/sources.list.d/openresty.list
 
-cat << 'EOF' > /etc/openresty/nginx.conf
+cat <<'EOF' >/etc/openresty/nginx.conf
 user nobody nogroup;
 worker_processes 1;
 pid /var/run/openresty.pid;
